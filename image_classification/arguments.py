2159d381af6c559b17d7a50ec57a71a3fa044efb
WenjieWWJ <wenjiewang96@gmail.com>
2020-10-19 12:08:48 +0800

    first commit, fix version issues

---
+++ image_classification/arguments.py
@@ -0,0 +1,29 @@
+import sys
+import argparse
+
+def get_args(description, mode='train'):
+    parser = argparse.ArgumentParser(description = description)
+    parser.add_argument('-s', '--seed', type=int, help = 'Give random seed')
+    parser.add_argument('-d', '--dataset', choices = ['imagenette', 'imagewoof', 'cifar10'], help = 'Give the dataset name from the choices')
+    if mode == 'train':
+        parser.add_argument('-m', '--model', choices = ['resnet10', 'resnet14', 'resnet18', 'resnet20', 'resnet26'], help = 'Give the model name from the choices')
+        parser.add_argument('-e', '--epoch', type=int, help = 'Give number of epochs for training')
+        parser.add_argument('-p', '--percentage', type=int, help='Percentage of dataset to be used for training', default=None)
+        parser.add_argument('-g', '--gpu', choices=['0', '1', '2', '3', '4', '5', '6', '7', 'cpu'], help='which GPU (or CPU) to be used for training', default='0')
+        parser.add_argument('-a', '--api_key', type=str, help='Comet ML API key', default=None)
+        parser.add_argument('-w', '--workspace', type=str, help='Comet ML workspace name or ID', default=None)
+
+    elif mode == 'eval':
+        parser.add_argument('-m', '--model', choices = ['resnet10', 'resnet14', 'resnet18', 'resnet20', 'resnet26'], help = 'Give the model name from the choices')
+        parser.add_argument('-g', '--gpu', choices=[0, 1, 'cpu'], help='which GPU (or CPU) to be used for training', default=0)
+        parser.add_argument('-p', '--percentage', type=int, help='Percentage of dataset to be used for training', default=None)
+        parser.add_argument('-e', '--experiment', choices=['stagewise-kd', 'traditional-kd', 'simultaneous-kd', 'no-teacher', 'fsp-kd', 'attention-kd'])
+
+    elif mode == 'data':
+        parser.add_argument('-p', '--percentage', type=int, help='Give percentage of dataset')
+
+    else: 
+        sys.exit(f'invalid mode: {mode}')
+
+    args = parser.parse_args()
+    return args
---
+++ image_classification/datasets/dataset.py
@@ -0,0 +1,35 @@
+from fastai.vision import *
+# from fastai.vision.all import get_transforms, untar_data, URLs
+# from fastai.vision.data import untar_data, URLs, ImageDataLoaders
+# from fastai.vision.augment import aug_transforms
+# from fastai.vision.core import cifar_stats
+
+
+def get_dataset(dataset, batch_size, percentage=None):
+    val = 'val'
+    sz = 224
+#     stats = imagenet_stats
+    if dataset == 'imagenette' : 
+        path = untar_data('https://s3.amazonaws.com/fast-ai-imageclas/imagenette')
+        classes = ['n01440764', 'n02102040', 'n02979186', 'n03000684', 'n03028079', 'n03394916', 'n03417042', 'n03425413', 'n03445777', 'n03888257']
+    elif dataset == 'cifar10' : 
+        path = untar_data(URLs.CIFAR)
+        classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    elif dataset == 'imagewoof' : 
+        path = untar_data('https://s3.amazonaws.com/fast-ai-imageclas/imagewoof')
+        classes = ['n02086240', 'n02087394', 'n02088364', 'n02089973', 'n02093754', 'n02096294', 'n02099601', 'n02105641', 'n02111889', 'n02115641']
+    else:
+        sys.exit(f'invalid dataset : {dataset}')
+    
+    if percentage is not None:
+        path = path/('new' + str(percentage))
+
+    tfms = get_transforms(do_flip=False)
+#     tfms = aug_transforms(do_flip=False)
+    if dataset == 'cifar10' : 
+        val = 'test'
+        sz = 32
+        stats = cifar_stats
+
+    return ImageDataBunch.from_folder(path, train='train', valid=val, bs=batch_size, size=sz, ds_tfms=tfms, classes=classes).normalize(stats)
+#     return ImageDataLoaders.from_folder(path, train='train', valid=val, bs=batch_size, size=sz, ds_tfms=tfms, classes=classes).normalize(stats)
